40a2e7a473e76cc2c2479195f81aaecb1fd208c3
dami_codes <80917622+Rotimi1234@users.noreply.github.com>
2022-05-27 22:43:22 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * navbar variables
+ */
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const heder = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+  heder.classList.toggle("active");
+});
+
+
+
+const sr = ScrollReveal ({
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  opacity: 0.2,
+  reset: true
+})
+
+sr.reveal('.hero-title, .logo, .section-text, .features-content-subtitle, .hero-text, .about-content, .section-title, .section-text',{delay: 400, origin: 'left'})
+
+sr.reveal('.form-text, .footer-link, .contact-list, .features-content-text, .features-content-title, .hero-form, .features-banner',{delay: 400, origin: 'right'})
+
+sr.reveal('.hero-banner, .social-link, .footer-text, .contact-form, .blog-card, .features-list, .btn-group, .about-card',{delay: 400, origin: 'bottom'})
+
+sr.reveal('.footer-link-box',{delay: 400, origin: 'top'})
+
