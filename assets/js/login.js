ba303e22875db7fa1faca7a107ac5452c0623ae5
dami_codes <80917622+Rotimi1234@users.noreply.github.com>
2022-05-27 23:53:06 +0100

    Add files via upload

---
+++ assets/js/login.js
@@ -0,0 +1,81 @@
+
+            // Import the functions you need from the SDKs you need
+            import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+            import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-analytics.js";
+            
+            const firebaseConfig = {
+              apiKey: "AIzaSyBdLJlK7W-WBJqm4KrVHXPw5tBrbScV-dE",
+              authDomain: "login-db-70f93.firebaseapp.com",
+              databaseURL: "https://login-db-70f93-default-rtdb.firebaseio.com",
+              projectId: "login-db-70f93",
+              storageBucket: "login-db-70f93.appspot.com",
+              messagingSenderId: "527342224730",
+              appId: "1:527342224730:web:5dca29c8d48bc73bf6df63",
+              measurementId: "G-FXQPLKEGZR"
+            };
+          
+            // Initialize Firebase
+            const app = initializeApp(firebaseConfig);
+            const analytics = getAnalytics(app);
+    
+            import { getDatabase, ref, set, child, get, update, remove }
+            from "https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js"
+    
+            const db = getDatabase();
+
+            //-----------------------------The reference------------------------------//
+            
+            const username = document.getElementById("username");
+            const pass = document.getElementById("pass");
+            const submit = document.getElementById("sub-btn");
+
+
+            // Authenticate process//
+        function AuthenticateUser(){
+            const dbref = ref(db);
+
+            get(child(dbref, "UsersList/"+ username.value)).then((snapshot)=>{
+                if(snapshot.exists()){
+                    alert("login Successful")
+                    let dbpass = decPass(snapshot.val().password);
+                    if(dbpass == pass.value){
+                        login(snapshot.val());
+                    }
+
+                    else{
+                        alert("username or passwod is invalid")
+                    }
+                
+                }
+                    else{
+                        alert("username or password is invalid");
+                    }    
+            });
+        }
+
+        // -----------------------Decryption process------------------------//
+        function decPass(dbpass){
+                var pass12 = CryptoJS.AES.decrypt(dbpass, pass.value);
+                return pass12.toString(CryptoJS.enc.Utf8);
+            }
+        
+            // ----------------------Login------------------------//
+            function login(user){
+                let keepLoggedIn = document.getElementById("customswitch1").checked;
+
+                if (!keepLoggedIn) {
+                    sessionStorage.setItem('user', JSON.stringify(user));
+                    window.location="index.html"
+                }
+
+                else{
+                    localStorage.setItem('keepLoggedIn', 'yes');
+                    localStorage.setItem('user', JSON.stringify(user));
+                    window.location="index.html"
+                }
+            }
+
+            // ----------------------------------Assign the event-------------------//
+            submit.addEventListener('click', AuthenticateUser)
+
+      
