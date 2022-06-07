cb3016a6de141041ce252719ee609af77e32a6f3
dami_codes <80917622+Rotimi1234@users.noreply.github.com>
2022-06-07 17:01:43 +0100

    Update pay.js

--- assets/js/pay.js
+++ assets/js/pay.js
@@ -7,6 +7,105 @@ const emailval = document.getElementsByClassName("emailval").textContent;
  
 console.log("Payment Working")
 
+
+function makePayment() {
+
+const payername = document.getElementById("userlink").textContent;
+const emailval = document.getElementsByClassName("emailval").textContent;
+ 
+console.log("Payment Working")
+
+    FlutterwaveCheckout({
+      public_key: "FLWPUBK_TEST-fa35fc033f99874e75ce5688e03cccb5-X",
+      tx_ref: Math.random(),
+      amount: 54600,
+      currency: "NGN",
+      payment_options: "card, banktransfer, ussd",
+      redirect_url: "https://guileless-stroopwafel-64f468.netlify.app/rickroll.html",
+      meta: {
+        consumer_id: Math.floor(Math.random() * 100001),
+      },
+      customer: {
+        email: emailval,
+        phone_number: "08102909304",
+        name: payername,
+      },
+      customizations: {
+        title: "Basic Plan",
+        description: "Payment for an awesome cruise",
+        logo: "https://guileless-stroopwafel-64f468.netlify.app/assets/images/logos.svg",
+      },
+    });
+  }
+
+
+
+
+
+  
+
+function makePayment2() {
+
+    const payername = document.getElementById("userlink").textContent;
+    const emailval = document.getElementsByClassName("emailval").textContent; 
+    console.log("Payment Working")
+    
+        FlutterwaveCheckout({
+          public_key: "FLWPUBK_TEST-fa35fc033f99874e75ce5688e03cccb5-X",
+          tx_ref: Math.random(),
+          amount: 54600,
+          currency: "NGN",
+          payment_options: "card, banktransfer, ussd",
+          redirect_url: "https://guileless-stroopwafel-64f468.netlify.app/rickroll.html",
+          meta: {
+            consumer_id: Math.floor(Math.random() * 100001),
+          },
+          customer: {
+            email: emailval,
+            phone_number: "08102909304",
+            name: payername,
+          },
+          customizations: {
+            title: "Professional Plan",
+            description: "Payment for an awesome cruise",
+            logo: "https://guileless-stroopwafel-64f468.netlify.app/assets/images/logos.svg",
+          },
+        });
+      }
+
+
+
+
+      
+
+function makePayment3() {
+
+    const payername = document.getElementById("userlink").textContent;
+    const emailval = document.getElementsByClassName("emailval").textContent; 
+    console.log("Payment Working")
+    
+        FlutterwaveCheckout({
+          public_key: "FLWPUBK_TEST-fa35fc033f99874e75ce5688e03cccb5-X",
+          tx_ref: Math.random(),
+          amount: 54600,
+          currency: "NGN",
+          payment_options: "card, banktransfer, ussd",
+          redirect_url: "https://guileless-stroopwafel-64f468.netlify.app/rickroll.html",
+          meta: {
+            consumer_id: Math.floor(Math.random() * 100001),
+          },
+          customer: {
+            email: emailval,
+            phone_number: "08102909304",
+            name: payername,
+          },
+          customizations: {
+            title: "Enterprise Plan",
+            description: "Payment for an awesome cruise",
+            logo: "https://guileless-stroopwafel-64f468.netlify.app/assets/images/logos.svg",
+          },
+        });
+      }
     FlutterwaveCheckout({
       public_key: "FLWPUBK_TEST-fa35fc033f99874e75ce5688e03cccb5-X",
       tx_ref: Math.random(),
